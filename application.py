5faf4a937c1ab088d2bfe0268759cb8a017cbb0e
Frank Huff <fs-j@live.com>
2020-01-17 09:05:44 -0800

    added user model

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_script.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app():
   app = Flask(__name__)
 
   app.config.from_pyfile('settings.py')
+
+  db.init_app(app)
 
   from user.views import user_app
   app.register_blueprint(user_app)
