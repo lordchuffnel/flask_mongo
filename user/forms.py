60cf97b585969b91465141e53b83152dd27d10df
Frank Huff <fs-j@live.com>
2020-01-20 09:39:53 -0800

    im done

--- user/forms.py
+++ user/forms.py
@@ -1,54 +1,69 @@
-import re
 from flask_wtf import Form
-from wtforms import PasswordField, StringField, validators
-from wtforms.fields.html5 import EmailField
+from wtforms import validators, StringField, PasswordField
 from wtforms.widgets import TextArea
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
+import re
 
 from user.models import User
 
-
 class BaseUserForm(Form):
-  first_name = StringField('First Name', [validators.Required()])
-  last_name = StringField('Last Name', [validators.Required()])
-  email = EmailField('Email Address', [
-    validators.DataRequired(),
-    validators.Email()
-  ])
-  username = StringField('Username', [
-    validators.Required(),
-    validators.length(min=4, max=25)
-  ])
-  bio = StringField('Bio', widget=TextArea(), validators=[validators.Length(max=60)])
-
-class RegisterForm(BaseUserForm):
-  password = PasswordField('New Password', [
-    validators.Required(),
-    validators.EqualTo('confirm', message='Passwords must match'),
-    validators.length(min=4, max=80)
-  ])
-  confirm = PasswordField('Repeat Password')
-
-  def validate_username(form, field):
-    if User.objects.filter(username=field.data).first():
-      raise ValidationError('Username already exists')
-    if not re.match("^[a-zA-z0-9_-]{4,25}$", field.data):
-      raise ValidationError("Invalid username")
-
-  def validate_email(form, field):
-    if User.objects.filter(email=field.data).first():
-      raise ValidationError("Email is already in use")
+    first_name = StringField('First Name', [validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.DataRequired()])
+    email = EmailField('Email address', [
+        validators.DataRequired(),
+        validators.Email()
+        ]
+    )
+    username = StringField('Username', [
+        validators.DataRequired(),
+        validators.length(min=4, max=25)
+        ])
+    bio = StringField('Bio',
+        widget=TextArea(),
+        validators=[validators.Length(max=160)]
+    )
+        
+class PasswordBaseForm(Form):
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.length(min=4, max=80)
+        ])
+    confirm = PasswordField('Repeat Password')
+    
+class RegisterForm(BaseUserForm, PasswordBaseForm):
+    def validate_username(form, field):
+        if User.objects.filter(username=field.data).first():
+            raise ValidationError("Username already exists")
+        if not re.match("^[a-zA-Z0-9_-]{4,25}$", field.data):
+            raise ValidationError("Invalid username")
+            
+    def validate_email(form, field):
+        if User.objects.filter(email=field.data).first():
+            raise ValidationError("Email is already in use")
 
 class LoginForm(Form):
-  username = StringField('Username', [
-    validators.DataRequired(),
-    validators.length(min=4, max=25)
-  ])
-
-  password = PasswordField('Password', [
-    validators.DataRequired(),
-    validators.length(min=4, max=80)
-  ])
-
+    username = StringField('Username', [
+        validators.DataRequired(),
+        validators.length(min=4, max=25)
+        ])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.length(min=4, max=80)
+        ])
+        
 class EditForm(BaseUserForm):
-  pass
+    pass
+
+class ForgotForm(Form):
+    email = EmailField('Email address',
+        [validators.DataRequired(), validators.Email()]
+    )
+    
+class PasswordResetForm(PasswordBaseForm):
+    current_password = PasswordField('Current Password',
+        [validators.DataRequired(),
+        validators.Length(min=4, max=80)]
+    )
+    
--- user/views.py
+++ user/views.py
@@ -1,86 +1,194 @@
-import bcrypt
 from flask import Blueprint, render_template, request, redirect, session, url_for, abort
+import bcrypt
+import uuid
 
-from user.forms import RegisterForm, LoginForm, EditForm
 from user.models import User
+from user.forms import RegisterForm, LoginForm, EditForm, ForgotForm, PasswordResetForm
+from utilities.common import email
 
 user_app = Blueprint('user_app', __name__)
-
+    
 @user_app.route('/register', methods=('GET', 'POST'))
 def register():
-  form = RegisterForm()
-  if form.validate_on_submit():
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(form.password.data, salt)
-    user = User(
-      username=form.username.data,
-      password=form.password.data,
-      email=form.email.data,
-      first_name=form.first_name.data,
-      last_name=form.last_name.data
-    )
-    user.save()
-    return "User registered"
-  return render_template('user/register.html', form=form)
-
+    form = RegisterForm()
+    if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(form.password.data, salt)
+        code = str(uuid.uuid4())
+        user = User(
+            username=form.username.data,
+            password=hashed_password,
+            email=form.email.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            change_configuration={
+                "new_email": form.email.data.lower(),
+                "confirmation_code": code
+                }
+            )
+            
+        # email the user
+        body_html = render_template('mail/user/register.html', user=user)
+        body_text = render_template('mail/user/register.txt', user=user)
+        email(user.email, "Welcome to Flaskbook", body_html, body_text)
+        
+        user.save()
+        return "User registered"
+    return render_template('user/register.html', form=form)
+    
 @user_app.route('/login', methods=('GET', 'POST'))
 def login():
-  form = LoginForm()
-  error = None
-
-  if form.validate_on_submit():
-    user = User.objects.filter(
-      username = form.username.data
-    ).first()
-    if user:
-      if bcrypt.hashpw(form.password.data, user.password) == user.password:
-        session['username'] = form.username.data
-        return "User logged in"
-      else:
-        user = None
-    if not user:
-      error = 'Incorrect credentials'
-  return render_template('user/login.html', form=form, error=error)
-
+    form = LoginForm()
+    error = None
+    
+    if request.method == 'GET' and request.args.get('next'):
+        session['next'] = request.args.get('next')
+        
+    if form.validate_on_submit():
+        user = User.objects.filter(
+            username=form.username.data
+            ).first()
+        if user:
+            if bcrypt.hashpw(form.password.data, user.password) == user.password:
+                session['username'] = form.username.data
+                if 'next' in session:
+                    next = session.get('next')
+                    session.pop('next')
+                    return redirect(next)
+                else:
+                    return 'User logged in'
+            else:
+                user = None
+        if not user:
+            error = 'Incorrect credentials'
+    return render_template('user/login.html', form=form, error=error)
+    
 @user_app.route('/logout', methods=('GET', 'POST'))
 def logout():
-  session.pop('username')
-  return redirect(url_for('user_app.login'))
-
+    session.pop('username')
+    return redirect(url_for('user_app.login'))
+    
 @user_app.route('/<username>', methods=('GET', 'POST'))
 def profile(username):
-  edit_profile = False
-  user = User.objects.filter(username=username).first()
-  if session.get('username') and user.username == session.get('username'):
-    edit_profile = True
-  if user:
-    return render_template('user/profile.html, user=user, edit_profile=edit_profile)
-  else:
-    abort(404)
-    
-@ user_app.route('/edit', methods=("GET", "POST"))
+    edit_profile = False
+    user = User.objects.filter(username=username).first()
+    if session.get('username') and user.username == session.get('username'):
+        edit_profile = True
+    if user:
+        return render_template('user/profile.html', user=user, edit_profile=edit_profile)
+    else:
+        abort(404)
+        
+@user_app.route('/edit', methods=('GET', 'POST'))
 def edit():
-  error = None
-  message = None
-  user = User.objects.filter(username=session.get('username')).first()
-  if user:
-    form = EditForm(obj=user)
+    error = None
+    message = None
+    user = User.objects.filter(username=session.get('username')).first()
+    if user:
+        form = EditForm(obj=user)
+        if form.validate_on_submit():
+            if user.username != form.username.data.lower():
+                if User.objects.filter(username=form.username.data.lower()).first():
+                    error = "Username already exists"
+                else:
+                    session['username'] = form.username.data.lower()
+                    form.username.data = form.username.data.lower()
+            if user.email != form.email.data.lower():
+                if User.objects.filter(email=form.email.data.lower()).first():
+                    error = "Email already exists"
+                else:
+                    code = str(uuid.uuid4())
+                    
+                    user.change_configuration = {
+                        "new_email": form.email.data.lower(),
+                        "confirmation_code": code
+                    }
+                    user.email_confirmed = False
+                    form.email.data = user.email
+                    message = "You will need to confirm the new email to complete this change"
+                    
+                    # email the user
+                    body_html = render_template('mail/user/change_email.html', user=user)
+                    body_text = render_template('mail/user/change_email.txt', user=user)
+                    email(user.change_configuration['new_email'], "Confirm your new email", body_html, body_text)
+                    
+            if not error:
+                form.populate_obj(user)
+                user.save()
+                if not message:
+                    message = "Profile updated"
+    
+        return render_template("user/edit.html", form=form, error=error, message=message)
+    else:
+        abort(404)
+        
+@user_app.route('/confirm/<username>/<code>', methods=('GET', 'POST'))
+def confirm(username, code):
+    user = User.objects.filter(username=username).first()
+    if user and user.change_configuration and user.change_configuration.get('confirmation_code'):
+        if code == user.change_configuration.get('confirmation_code'):
+            user.email = user.change_configuration.get('new_email')
+            user.change_configuration = {}
+            user.email_confirmed = True
+            user.save()
+            return render_template('user/email_confirmed.html')
+    else:
+        abort(404)
+     
+@user_app.route('/forgot', methods=('GET', 'POST'))
+def forgot():
+    error = None
+    message = None
+    form = ForgotForm()
     if form.validate_on_submit():
-      if user.username != form.username.data:
-        if User.objects.filter(username=form.username.data.lower()).first():
-          error = "username already exists"
-        else:
-          session['username'] = form.username.data.lower()
-          form.username.data = form.username.data.lower()
-      if user.email != form.email.data:
-        if User.objects.filter(email=form.email.data.lower()).first():
-          error = "email already exists"
-        else:
-          form.email.data = form.email.data.lower()
-      if not error:
-        form.populate_obj(user)
-        user.save()
-        message = "Profile updated"
-    return render_template('user/edit.html', form=form, error=error, message=message)
-  else:
-    abort(404)
+        user = User.objects.filter(email=form.email.data.lower()).first()
+        if user:
+            code = str(uuid.uuid4())
+            user.change_configuration={
+                "password_reset_code": code
+            }
+            user.save()
+            
+            # email the user
+            body_html = render_template('mail/user/password_reset.html', user=user)
+            body_text = render_template('mail/user/password_reset.txt', user=user)
+            email(user.email, "Password reset request", body_html, body_text)
+            
+        message = "You will receive a password reset email if we find that email in our system"
+    return render_template('user/forgot.html', form=form, error=error, message=message)
+    
+@user_app.route('/password_reset/<username>/<code>', methods=('GET', 'POST'))
+def password_reset(username, code):
+    message = None
+    require_current = None
+    
+    form = PasswordResetForm()
+    
+    user = User.objects.filter(username=username).first()
+    if not user or code != user.change_configuration.get('password_reset_code'):
+        abort(404)
+        
+    if request.method == 'POST':
+        del form.current_password
+        if form.validate_on_submit():
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(form.password.data, salt)
+            user.password = hashed_password
+            user.change_configuration = {}
+            user.save()
+            
+            if session.get('username'):
+                session.pop('username')
+            return redirect(url_for('user_app.password_reset_complete'))
+            
+    return render_template('user/password_reset.html',
+        form=form,
+        message=message,
+        require_current=require_current,
+        username=username,
+        code=code
+    )
+    
+@user_app.route('/password_reset_complete')
+def password_reset_complete():
+    return render_template('user/password_change_confirmed.html')
